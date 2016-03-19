c6364a7cb9ea26e189aee5461e22875d91022eac
Hyde Zhang <Phoenix_520_1227@hotmail.com>
2016-03-19 15:03:24 +0000

    initial commit for Two Sum

---
+++ Two Sum/solution.py
@@ -0,0 +1,8 @@
+print 'hello world'
+
+if __name__ == "__main__":
+	print 'This is the main check\n When the Python interpreter reads a source file, \n' \
+	'it executes all of the code found in it. Before executing the code, it will define a few special variables. \n' \
+	' For example, if the python interpreter is running that module (the source file) as the main program, it sets the \n' \
+	'special __name__ variable to have a value "__main__". If this file is being imported from another module, __name__ will \n' \
+	'be set to the module\'s name.' 
