3e8107c53fbdbe87e28a859b93edb2b902165b17
Hyde Zhang <Phoenix_520_1227@hotmail.com>
2016-10-31 18:59:24 +0000

    Switch to 0001

---
+++ 0232Implement_Queue_using_Stacks/MyQueue.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+class MyQueue {
+    Stack myStack = new Stack();
+    // Push element x to the back of queue.
+    public void push(int x) {
+        
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        
+    }
+
+    // Get the front element.
+    public int peek() {
+        
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        
+    }
+}
