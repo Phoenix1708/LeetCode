b17799bb60fd436cd94ae7f12717224820f1567b
Hyde Zhang <Phoenix_520_1227@hotmail.com>
2016-11-07 21:14:28 +0000

    Python solution

--- 0155_Min_Stack/MinStack.py
+++ 0155_Min_Stack/MinStack.py
@@ -1,40 +1,45 @@
+import sys
+
+
 class MinStack(object):
 
     def __init__(self):
         """
         initialize your data structure here.
         """
-        
+        self.stack = list()
+        self.min = sys.maxint
 
     def push(self, x):
         """
         :type x: int
         :rtype: void
         """
-        
+        if x <= self.min:
+            # store current min in order to update
+            # when minimal number is popped
+            if not len(self.stack) == 0:
+                self.stack.append(self.min)
+            self.min = x
+
+        self.stack.append(x)
 
     def pop(self):
         """
         :rtype: void
         """
-        
+        if not len(self.stack) == 0:
+            if self.min == self.stack.pop():
+                    self.min = self.stack.pop() if not len(self.stack) == 0 else sys.maxint
 
     def top(self):
         """
         :rtype: int
         """
-        
+        return self.stack[len(self.stack) - 1]
 
     def getMin(self):
         """
         :rtype: int
         """
-        
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(x)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+        return self.min
