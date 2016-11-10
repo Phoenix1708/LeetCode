10f446f63753d91d318dce1bf0bc5ad2899dfd9c
Hyde Zhang <xueshi.x.zhang@accenture.com>
2016-11-10 14:53:07 +0000

    Missed previous python semi-quick-sort solution

--- 0215_Kth_Largest_Element_in_an_Array/Solution.py
+++ 0215_Kth_Largest_Element_in_an_Array/Solution.py
@@ -11,32 +11,35 @@ class Solution(object):
         	return nums[0]
 
         if len(nums) == 2:
-        	return nums[0] if nums[k - 1] > nums[0] else nums[1]
+        	# find the kth element compare with either first or second
+        	return nums[k - 1] if nums[k - 1] < nums[0] else nums[0]
 
         left = 0;
         right = len(nums) - 1
-        while true:
-        	pos = partition(nums, left, right)
+        while True:
+        	pos = self.partition(nums, left, right)
         	if pos == k - 1:
         		return nums[pos]
         	# the rank of the element we want is higher
         	# than the ranking of pivot, we search left
-        	if pos > k - 1:
+        	elif pos > k - 1:
         		right = pos - 1
-            else: 
-                left =  pos + 1
-
-     def partition(self, nums, left, right):
+        	else:
+        	    left =  pos + 1
+        	    
+    def partition(self, nums, left, right):
      	# pick left as pivot
      	l = left + 1
      	r = right
      	while l <= r:
-     		if nums[l] < nums[left] && nums[r] > nums[left]:
+     		if nums[l] < nums[left] and nums[r] > nums[left]:
      			# swap
-     			nums[l], nums[r] = nums[l++], nums[r--]
-
-     		if nums[l] > nums[left]: l++
-     		if nums[r] < nums[right]: r--
+     			nums[l], nums[r] = nums[r], nums[l]
+     			l += 1
+     			r -= 1
+     			
+     		if nums[l] >= nums[left]: l += 1
+     		if nums[r] <= nums[left]: r -= 1
      	# swap pivot
      	nums[left], nums[r] = nums[r], nums[left]
-     	return r     	
+     	return r
